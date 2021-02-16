20bdce3bafa532fc57ea6fb53d32e7c180d14b8e
Jean Paul | Origem <jeanpaul2022@gmail.com>
2021-02-16 02:35:14 -0300

    M  -  Reformulando page Home e adicionando nova pageCarousel

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  View,
+  Image,
+  Text,
+  Pressable,
+} from 'react-native';
+import {Actions} from 'react-native-router-flux';
+
+const listPageDestino = [
+  {
+    text: 'Carousel ',
+    uri:
+      'https://cdn.dribbble.com/users/346110/screenshots/10784815/media/d9aef95460878d2ed58fcf6faa3babc0.jpg',
+  },
+];
 
 const Circle = () => {
   const border = 200;
@@ -8,22 +25,47 @@ const Circle = () => {
       style={{
         width: border,
         height: border,
-        backgroundColor: 'red',
+        backgroundColor: 'blue',
         borderRadius: border,
       }}
     />
   );
 };
 
+const PageDestino = ({item}: any) => {
+  return (
+    <Pressable
+      onPress={() => Actions.pageCarousel()}
+      style={{
+        marginTop: 16,
+        marginHorizontal: 16,
+        height: 100,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Image
+        source={{uri: item.uri}}
+        style={{
+          borderRadius: 16,
+          ...(StyleSheet.absoluteFill as {}),
+        }}
+      />
+      <Text style={{color: 'white', fontSize: 48, fontWeight: 'bold'}}>
+        {item.text}
+      </Text>
+    </Pressable>
+  );
+};
+
 const Home = () => {
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignSelf: 'center',
-      }}>
-      <Circle />
+    <SafeAreaView>
+      <FlatList
+        keyExtractor={(item, index) => String(index)}
+        data={listPageDestino}
+        renderItem={PageDestino}
+      />
     </SafeAreaView>
   );
 };
--- src/router.js
+++ src/router.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {Router, Stack, Scene} from 'react-native-router-flux';
 import Home from './page/Home';
+import PageCarousel from './page/PageCarousel';
 
 const RouterProvider = () => {
   return (
     <Router>
       <Stack key="root">
-        {/* <Scene key="login" component={Login} title="Login" /> */}
-        {/* <Scene key="register" component={Register} title="Register" /> */}
-        <Scene key="home" component={Home} title="Home" />
+        <Scene key="home" component={Home} title="Home" hideNavBar />
+        <Scene
+          key="pageCarousel"
+          component={PageCarousel}
+          navTransparent
+          back
+          backButtonTintColor="#FFF"
+        />
       </Stack>
     </Router>
   );
